f778c5027cdb6d8bff98cc964f94bd4f1b6b038a
Fay Carsons <faycarsons23@gmail.com>
2024-03-15 14:10:50 -0400

    add nearest fn to KDTree

--- lib/tree.ml
+++ lib/tree.ml
@@ -354,9 +354,12 @@ module KDTree (E : ElementN) : KDTree with type elt = E.t = struct
   type tree = Node of elt * tree * tree | Leaf of elt list | Empty
   type t = { capacity : int; dimensionality : int; tree : tree }
 
+  let leaf points = Leaf points
+
   module FA = Float.Array
 
-  let leaf points = Leaf points
+  let distance p1 p2 =
+    FA.map2 (fun a b -> (a -. b) ** 2.) p1 p2 |> FA.fold_left ( +. ) 0.
 
   let compare_dim dim e1 e2 =
     let p1, p2 = (E.position e1, E.position e2) in
@@ -412,4 +415,40 @@ module KDTree (E : ElementN) : KDTree with type elt = E.t = struct
       | Empty -> Leaf [ elt ]
     in
     { t with tree = insert' 0 tree }
+
+  let nearest t query =
+    let fold_children best_opt elt =
+      let pos = E.position elt in
+      let dist = distance query pos in
+      match best_opt with
+      | Some (_, curr_dist) as opt when curr_dist < dist -> opt
+      | _ -> Some (elt, dist)
+    in
+    let rec nearest' depth best = function
+      | Empty -> best
+      | Leaf elts -> List.fold_left fold_children best elts
+      | Node (median, left, right) ->
+          let dim = depth mod t.dimensionality in
+          let point = E.position median in
+          let query_d, point_d =
+            FA.(unsafe_get query dim, unsafe_get point dim)
+          in
+          let go_left = query_d < point_d in
+          let closer, further =
+            if go_left then (left, right) else (right, left)
+          in
+          let curr_best =
+            match best with
+            | None -> Some (median, distance query point)
+            | _ -> best
+          in
+          let curr_best = nearest' (succ depth) curr_best closer in
+
+          Option.bind curr_best (fun curr ->
+              if (query_d -. point_d) ** 2. < snd curr then
+                nearest' (succ depth) (Some curr) further
+              else Some curr)
+    in
+
+    Option.map fst @@ nearest' 0 None t.tree
 end

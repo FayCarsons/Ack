7a7273d08861fe73440f9714823e2e7b64a18f5c
Fay Carsons <faycarsons23@gmail.com>
2024-03-15 17:25:50 -0400

    add octree and kdtree to lib exports

--- lib/lib.ml
+++ lib/lib.ml
@@ -1,4 +1,7 @@
 module Quadtree = Tree.Quadtree
+module Octree = Tree.Octree
+module KDTree = Tree.KDTree
 
 module type Element2D = Tree_intf.Element2D
 module type Element3D = Tree_intf.Element3D
+module type ElementN = Tree_intf.ElementN
--- lib/lib.mli
+++ lib/lib.mli
@@ -3,3 +3,8 @@ module type Element3D = Tree_intf.Element3D
 
 module Quadtree (N : Tree_intf.Scalar) (E : Element2D with type n = N.t) :
   Tree_intf.Quadtree with type n = N.t and type elt = E.t
+
+module Octree (N : Tree_intf.Scalar) (E : Element3D with type n = N.t) :
+  Tree_intf.Octree with type n = N.t and type elt = E.t
+
+module KDTree (E : Tree_intf.ElementN) : Tree_intf.KDTree with type elt = E.t
